191263e492bc81c7c082491dab54f34e1b98bc14
Antoine Guillaume <01guillaume.antoine@gmail.com>
2015-05-29 14:37:14 +0200

    Update bdd_connexion.php

--- espace-membre/bdd_connexion.php
+++ espace-membre/bdd_connexion.php
@@ -2,7 +2,6 @@
 
 try{
   $bdd = new PDO('mysql:host=127.0.0.1;dbname=espace_membre', 'root', 'root');
-  //$bdd = new PDO('mysql:host=antoineguillaume.be.mysql;dbname=antoineguillaum', 'antoineguillaum', 'Dx3vkRH6');
 }catch(PDOException $e){
   echo $e->getMessage();
 } 
